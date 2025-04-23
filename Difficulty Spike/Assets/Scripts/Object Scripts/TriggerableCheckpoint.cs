289262b85d5ec5f88a3a76abce568ea12db23eba
plzzwork <lohith.lingutla@gmail.com>
2025-04-23 14:27:10 -0400

    changes to Lv1 and some changes to some scripts

--- Difficulty Spike/Assets/Scripts/Object Scripts/TriggerableCheckpoint.cs
+++ Difficulty Spike/Assets/Scripts/Object Scripts/TriggerableCheckpoint.cs
@@ -2,11 +2,17 @@ using UnityEngine;
 
 public class TriggerableCheckpoint : MonoBehaviour
 {
-    void OnTriggerEnter2D(Collider2D other)
-    {
-
+    private SpriteRenderer sprite;
+    void OnTriggerEnter2D(Collider2D other){
         if (other.TryGetComponent<PlayerDeathManager>(out PlayerDeathManager pdm)){
             pdm.Checkpoint(transform);
+            Color reed = Color.red;
+            reed.a = 0.38f;
+            sprite.color = reed;
         }
+    }
+
+    void Start(){
+        sprite = GetComponent<SpriteRenderer>();
     }
 }
