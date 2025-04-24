49852b262f11874379c546d6c71e5ce8caee057d
plzzwork <lohith.lingutla@gmail.com>
2025-04-23 21:25:53 -0400

    Level2 changes, checkpoints, music, added ground collision, added camera

--- Difficulty Spike/Assets/Scripts/Object Scripts/TriggerableCheckpoint.cs
+++ Difficulty Spike/Assets/Scripts/Object Scripts/TriggerableCheckpoint.cs
@@ -6,6 +6,7 @@ public class TriggerableCheckpoint : MonoBehaviour
     void OnTriggerEnter2D(Collider2D other){
         if (other.TryGetComponent<PlayerDeathManager>(out PlayerDeathManager pdm)){
             pdm.Checkpoint(transform);
+            
             Color reed = Color.red;
             reed.a = 0.38f;
             sprite.color = reed;
